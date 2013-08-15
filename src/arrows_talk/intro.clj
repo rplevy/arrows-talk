79669953f0ffadc8edfe514758c0584d917d99bc
Robert P. Levy <r.p.levy@gmail.com>
2013-08-15 16:44:30 -0700

    add intro

---
+++ src/arrows_talk/intro.clj
@@ -0,0 +1,18 @@
+(comment
+
+  "Arrow macros" or "threading macros" as they are called ...
+
+  (and this has nothing to do threads in the
+        concurrency and
+        parallelism sense)
+
+  ... are:
+
+  1. a way of making nested calls more readable.
+
+
+  2. a testament to the uniqueness and power of macros in Lisp
+
+  languages. Arrows are a syntactic innovation in both the
+
+  language core and the userland of the Clojure community.)
